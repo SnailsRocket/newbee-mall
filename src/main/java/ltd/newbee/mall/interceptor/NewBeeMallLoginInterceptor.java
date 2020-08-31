594274a0352cd42327f42e44f2a64db3d5bf7d4f
xwxyx <1837039333@qq.com>
2020-08-31 22:18:37 +0800

    update constants and NewBeeMallLoginInterceptor with annotion

--- src/main/java/ltd/newbee/mall/interceptor/NewBeeMallLoginInterceptor.java
+++ src/main/java/ltd/newbee/mall/interceptor/NewBeeMallLoginInterceptor.java
@@ -20,12 +20,31 @@ import javax.servlet.http.HttpServletResponse;
  * newbee-mall系统身份验证拦截器
  * HandlerInterceptor 的详解 https://blog.csdn.net/zhibo_lv/article/details/81699360
  * 登录拦截、权限校验、防重复提交
+ * 重写HandlerInterceptor的三个方法，
  */
 @Component
 public class NewBeeMallLoginInterceptor implements HandlerInterceptor {
 
+    /**
+     *
+     * @param request
+     * @param response
+     * @param o   handler
+     * @return
+     * @throws Exception
+     * preHandle方法是进行处理器拦截用的，顾名思义，该方法将在Controller处理之前进行调用
+     *
+     * postHandle这个方法只会在当前这个Interceptor的preHandle方法返回值为true的时候才会执行。
+     * postHandle是进行处理器拦截用的，它的执行时间是在处理器进行处理之后，也就是在Controller的方法调用之后执行，
+     * 但是它会在DispatcherServlet进行视图的渲染之前执行，也就是说在这个方法中你可以对ModelAndView进行操作
+     *
+     * afterCompletion 该方法也是需要当前对应的Interceptor的preHandle方法的返回值为true时才会执行。
+     * 该方法将在整个请求完成之后，也就是DispatcherServlet渲染了视图执行。
+     * （这个方法的主要作用是用于清理资源的）
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+//        从请求中获取session，并get session中是否有 user 的 key ，登录过后对应每个user生成独有的key，放入session中，如果key为null，重定向到login界面，并return false，返回true就放行
         if (null == request.getSession().getAttribute(Constants.MALL_USER_SESSION_KEY)) {
             response.sendRedirect(request.getContextPath() + "/login");
             return false;
